b1a82d7d61cb59b05179005894e0c00ffe383cbc
NunoSempere <nuno.sempere@protonmail.com>
2023-03-15 15:30:05 -0600

    Revert "feat: add a more hardcore grid search"
    This reverts commit d32a23d3ac828128862a842fd841281f951957cc.

--- src/example.js
+++ src/example.js
@@ -5,6 +5,3 @@ console.log(result1)
 
 let result2 = find_beta_from_ci({ci_lower: 0.3, ci_upper: 0.8, ci_length: 0.95})
 console.log(result2)
-
-let result3 = find_beta_from_ci({ci_lower: "0.3", ci_upper: "0.8", ci_length: "0.95"})
-console.log(result3)
