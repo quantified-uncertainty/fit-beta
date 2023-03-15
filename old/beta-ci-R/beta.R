018a0a60cd39cdf54b773545b006471d487afdd1
NunoSempere <nuno.sempere@protonmail.com>
2023-03-15 01:20:09 -0600

    feat: add example test, delete old folder

--- old/beta-ci-R/beta.R
+++
@@ -1,8 +0,0 @@
-install.packages('bootComb')
-library(bootComb)
-
-getBetaFromCI(qLow=0.2,qUpp=0.9,alpha=0.1)
-# ^ get the parameters of a beta distribution
-# whose 90% (1-alpha) confidence interval
-# is 0.2 to 0.8
-dist1
--- test.js
+++ test.js
@@ -4,9 +4,9 @@ let ci_upper = 0.8
 
 let result = find_beta_from_ci({ci_lower, ci_upper})
 console.log(result)
+
 /* console.log([a,b])
 console.log(`beta(${a}, ${b})`)
-
 console.log(beta_cdf({x: ci_lower, a, b}))
 console.log(beta_cdf({x: ci_upper, a, b}))
 */
