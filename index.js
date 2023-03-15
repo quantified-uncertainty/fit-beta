60be886b13c6f16c9c5547a78744c02ed39a2dd2
NunoSempere <nuno.sempere@protonmail.com>
2023-03-15 00:59:35 -0600

    tweak: get working version.

--- index.js
+++ index.js
@@ -10,5 +10,60 @@ import betainc from '@stdlib/math-base-special-betainc/lib/index.js'
  *
  * To keep this semantic meaning in mind, I am making a copy of the function in mind.
  */
-const beta_cdf = ({x,a,b}) => betainc(x,a,b)
 
+const beta_cdf = ({x,a,b}) => betainc(x,a,b)
+/*
+// console.log(beta_cdf({x: 0.1, a: 1.0, b:25.0})) 
+// should be ~0.928
+// https://www.squiggle-language.com/playground#code=eNqrVirOyC8PLs3NTSyqVLIqKSpN1QELuaZkluQXwURSUtMSS3NKnPNTUpWslJJT0jSSUksSNQz1DHQUjEz1DBQ0dRQM9Aw1lWoBnIcZzA%3D%3D
+*/ 
+
+const find_beta_from_ci = ({ci_lower, ci_upper}) => {
+
+	function loss(x){
+		let a = x[0]
+		let b = x[1] 
+		let smoother = x => x**2 // Math.abs
+		let loss_ci_lower = smoother(beta_cdf({x: ci_lower, a, b}) - 0.05)
+		let loss_ci_upper = smoother(beta_cdf({x: ci_upper, a, b}) - 0.95)
+		// the advantage of the Math.abs is that it has a clearer interpretation
+		// (within x digits of the result)
+		// and that it doesn't become very small very soon
+		// the disadvantages is that it's slightly less differentiable
+		let result = loss_ci_lower + loss_ci_upper
+		// console.log(`loss: ${result}`)
+		return result
+	}
+
+	const h =0.0001
+	function gradient(x){ // not used, but useful for other optimization methods.
+		let a = x[0]
+		let b = x[1] 
+		let f = loss([a,b])
+		
+		let f_h_a = loss([a + h, b])
+		let f_h_b = loss([a, b+h])
+
+		let df_da = (f_h_a - f)/h
+		let df_db = (f_h_b - f)/h
+
+		return [df_da, df_db]
+	}
+
+	let x0 = [ 10, 5]
+	let result = nelderMead(loss, x0);
+	return [result.x[0], result.x[1]]
+	
+}
+
+let ci_lower = 0.3
+let ci_upper = 0.8
+
+let result = find_beta_from_ci({ci_lower, ci_upper})
+console.log(result)
+/* console.log([a,b])
+console.log(`beta(${a}, ${b})`)
+
+console.log(beta_cdf({x: ci_lower, a, b}))
+console.log(beta_cdf({x: ci_upper, a, b}))
+*/
